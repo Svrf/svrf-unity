c7afcf8845b6128a509fb0c7f40edd19103b896d
Roman Chaley <romanchaley@gmail.com>
2019-05-29 18:37:19 +0300

    Added unity project with included DLLs

---
+++ Assets/Scripts/SvrfModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityGLTF;
+
+public class SvrfModel : MonoBehaviour
+{
+    public string Id;
+
+    public async void Start()
+    {
+        gameObject.transform.SetParent(gameObject.transform);
+
+        // make API request
+        // var request = await api.GetById(Id);
+
+        var gltfComponent = gameObject.AddComponent<GLTFComponent>();
+        gltfComponent.GLTFUri = "https://www.svrf.com/storage/svrf-models/730861/gltf/StarGlassesBlender2.gltf"; // request.uri
+
+        await gltfComponent.Load();
+    }
+}
